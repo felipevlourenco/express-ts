674686aa67599f69727e359eef089477178d5b9c
Felipe Lourenco <felipe.v.lourenco@gmail.com>
2021-05-12 10:31:16 +0200

    Chore: Remove pdf part

--- src/app.ts
+++ src/app.ts
@@ -1,42 +1,38 @@
-// import dotenv from 'dotenv'
-// import express, { NextFunction, Request, Response } from 'express'
-// import ErrorHandler from './models/ErrorHandler'
-// import MasterRouter from './routes/MasterRouter'
-
-import createExampleTable from './utils/create'
+import dotenv from 'dotenv'
+import express, { NextFunction, Request, Response } from 'express'
+import ErrorHandler from './models/ErrorHandler'
+import MasterRouter from './routes/MasterRouter'
 
 // Load env vars from .env file
-// dotenv.config({
-//   path: '.env'
-// })
-// const PORT = process.env.PORT || 5000
+dotenv.config({
+  path: '.env'
+})
+const PORT = process.env.PORT || 5000
 
-// class Server {
-//   private _app = express()
-//   private _router = MasterRouter
+class Server {
+  private _app = express()
+  private _router = MasterRouter
 
-//   get app() {
-//     return this._app
-//   }
+  get app() {
+    return this._app
+  }
 
-//   get router() {
-//     return this._router
-//   }
-// }
+  get router() {
+    return this._router
+  }
+}
 
-// const server = new Server()
+const server = new Server()
 
-// server.app.use('/api', server.router)
-// server.app.use((err: ErrorHandler, req: Request, res: Response, next: NextFunction) => {
-//   res.status(err.statusCode || 500).json({
-//     status: 'error',
-//     statusCode: err.statusCode || 500,
-//     message: err.message || 'Internal server error'
-//   })
-// })
+server.app.use('/api', server.router)
+server.app.use((err: ErrorHandler, req: Request, res: Response, next: NextFunction) => {
+  res.status(err.statusCode || 500).json({
+    status: 'error',
+    statusCode: err.statusCode || 500,
+    message: err.message || 'Internal server error'
+  })
+})
 
-// server.app.listen(PORT, () => {
-//   console.log(`> Listening on port ${PORT}`)
-// })
-
-createExampleTable()
+server.app.listen(PORT, () => {
+  console.log(`> Listening on port ${PORT}`)
+})
